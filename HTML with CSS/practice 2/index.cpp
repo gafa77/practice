96be36ebf26833ff2472babf4adf01676d40bd4c
gafa77 <104118429+gafa77@users.noreply.github.com>
2024-06-26 16:50:21 +0530

    Add files via upload
    HTML & CSS Practice

---
+++ HTML with CSS/practice 2/index.cpp
@@ -0,0 +1,26 @@
+
+    using namespace std;
+
+int main()
+{
+    int arr[10], sum = 0;
+
+    // Accepting 10 integers in an array
+    cout << "Enter 10 integers: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arUHUHIUOHIUFTYTG8HIO   PJUIOHFVD  
+    
+        sum += arUHUHIUOHIUFTYTG8HIO   PJUIOHFVD  
+    
+    }
+
+    // Finding sum and average
+    float avg = (float)sum / 10;
+
+    // Displaying sum and average
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << avg << endl;
+
+    return 0;
+}
